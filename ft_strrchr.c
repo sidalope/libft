8c00679d0ada5ea291985262485d1be90dbeab35
Alessandro Bisiani <abisiani@c4a11c8.42berlin.de>
2025-05-15 18:49:45 +0200

    add: Add functioning strrchr and tests
    The substring finding functions need to be tested
    for edge cases.

---
+++ ft_strrchr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abisiani <abisiani@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/15 18:41:35 by abisiani          #+#    #+#             */
+/*   Updated: 2025/05/15 18:48:47 by abisiani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+
+	i = ft_strlen(s) - 1;
+	while(s[i])
+	{
+		if (s[i] == c)
+			return (char *)(s += i);
+		i--;
+	}
+	return (char *)(s += i);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: abisiani <abisiani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 15:15:17 by abisiani          #+#    #+#             */
-/*   Updated: 2025/05/15 18:25:59 by abisiani         ###   ########.fr       */
+/*   Updated: 2025/05/15 18:40:46 by abisiani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
-// ft_strrchr();
+char	*ft_strrchr(const char *s, int c);
 // ft_strncmp();
 // ft_memchr();
 // ft_memcmp();
