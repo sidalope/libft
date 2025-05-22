931f0c82585ab751793f6ff770ccb0ee9f0e286a
sidalope <bisi90@gmail.com>
2025-05-22 23:39:47 +0200

    add: Add strdup
    Only barest print testing performed.

---
+++ ft_strdup.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abisiani <abisiani@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/22 23:11:48 by abisiani          #+#    #+#             */
+/*   Updated: 2025/05/22 23:37:40 by abisiani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	size_t	i;
+	char	*new_string;
+
+	i = 0;
+	new_string = malloc(ft_strlen(s));
+	while (s[i])
+	{
+		new_string[i] = s[i];
+		i++;
+	}
+	new_string[i] = 0;
+	return (new_string);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: abisiani <abisiani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 15:15:17 by abisiani          #+#    #+#             */
-/*   Updated: 2025/05/22 22:18:59 by abisiani         ###   ########.fr       */
+/*   Updated: 2025/05/22 23:11:21 by abisiani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,10 @@ char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-char 	*ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strdup(const char *s);
 // ft_substr();
 // ft_strjoin();
 // ft_strtrim();
