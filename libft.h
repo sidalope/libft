e2a3f999adea7017058b06e1188cd087cf510b50
sidalope <bisi90@gmail.com>
2025-05-22 20:08:24 +0200

    add: Add functioning memchr and tests

--- libft.h
+++ libft.h
@@ -30,7 +30,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-// ft_memchr();
+void    *ft_memchr(const void *s, int c, size_t n);
 // ft_memcmp();
 // ft_strnstr();
 int		ft_atoi(const char *str);
