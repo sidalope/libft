59a6b3068bc0b0cc013b323abedae97fbd63061e
sidalope <bisi90@gmail.com>
2025-05-31 16:56:50 +0200

    wip: Refactor strlen and add non-tested strlcat
    strlcat is cursed beyond the pale. It cannot be
    known, even as it squints mockingly from the
    shadows.

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,39 +6,90 @@
 /*   By: abisiani <abisiani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 17:45:26 by abisiani          #+#    #+#             */
-/*   Updated: 2025/05/13 12:55:41 by abisiani         ###   ########.fr       */
+/*   Updated: 2025/05/15 15:33:12 by abisiani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
+#include "libft.h"
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <bsd/string.h>
 
-unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	unsigned int	len;
-	int				src_len;
-	int				dest_len;
 
-	len = 0;
-	src_len = 0;
-	while (dest[len])
-		len++;
-	dest_len = len;
-	while (src[src_len])
-		src_len++;
-	if (size <= len)
-		return (dest_len + src_len);
-	size = size - 1 - len;
-	while (*src && size)
+	size_t	i;
+	size_t	src_len;
+	size_t	dst_len;
+
+	i = 0;
+	src_len = ft_strlen(src);
+	dst_len = ft_strlen(dst);
+	if (!size && !dst)
+		return (src_len);
+	if (size <= dst_len)
+		return (size + src_len);
+	while (src[i] && (dst_len + i) < (size -1))
 	{
-		dest[len] = *src;
-		len++;
-		src++;
-		size--;
-		if (! *src)
-			break ;
+		dst[dst_len + i] = src[i];
+		i++;
 	}
-	dest[len] = '\0';
-	return (len);
+	dst[dst_len + i] = '\0';
+	return (dst_len + src_len);
+
+
+	// char		*d;
+	// const char	*s;
+	// (void)size;
+	// d = dst;
+	// s = src;
+	// d += ft_strlen(d);
+	// if (size >= ft_strlen(d) + ft_strlen(s) + 1)
+	// {
+	// 	while (*s)
+	// 	{
+	// 		*d++ = *s++;
+	// 		// printf("%c ", *d);
+	// 	}
+	// 	*d = '\0';
+	// }
+	// return (ft_strlen(d) + ft_strlen(s));
+
+	// while (size-- && s[i])
+	// {
+	// 	d[i] = s[i];
+	// 	i++;
+	// }
+	// i = 0;
+	// while (s[i])
+	// {
+	// 	i++;
+	// }
+	// return (i);
+
+	// unsigned int	len;
+	// int				src_len;
+	// int				dest_len;
+
+	// len = 0;
+	// src_len = 0;
+	// while (dest[len])
+	// 	len++;
+	// dest_len = len;
+	// while (src[src_len])
+	// 	src_len++;
+	// if (size <= len)
+	// 	return (dest_len + src_len);
+	// size = size - 1 - len;
+	// while (*src && size)
+	// {
+	// 	dest[len] = *src;
+	// 	len++;
+	// 	src++;
+	// 	size--;
+	// 	if (! *src)
+	// 		break ;
+	// }
+	// dest[len] = '\0';
+	// return (len);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: abisiani <abisiani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 15:15:17 by abisiani          #+#    #+#             */
-/*   Updated: 2025/05/15 12:01:22 by abisiani         ###   ########.fr       */
+/*   Updated: 2025/05/15 14:55:22 by abisiani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
-size_t	ft_strlen(char *s);
+size_t	ft_strlen(const char *s);
 void	*ft_memset(void *b, int c, size_t len);
 void	bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-// ft_strlcat();
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
 // ft_strchr();
 // ft_strrchr();
 // ft_strncmp();
@@ -45,7 +45,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 // ft_putstr_fd();
 // ft_putendl_fd();
 // ft_putnbr_fd();
-
 char	ft_tolower(char c);
 char	ft_toupper(char c);
 
