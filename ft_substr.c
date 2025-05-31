8897f2201b77cd5f2af02b0f2cb867217362527f
sidalope <bisi90@gmail.com>
2025-05-31 19:04:23 +0200

    add: Add ft_substr
    It allocates the max amount of memory it'll ever
    need which is too much. Could be better, much
    like my sleep.

--- ft_substr.c
+++ ft_substr.c
@@ -6,12 +6,11 @@
 /*   By: abisiani <abisiani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 13:08:42 by abisiani          #+#    #+#             */
-/*   Updated: 2025/05/23 22:51:51 by abisiani         ###   ########.fr       */
+/*   Updated: 2025/05/31 19:02:46 by abisiani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
-#include <stdlib.h>
+#include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -19,9 +18,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*ptr_substring;
 
 	i = 0;
-	ptr_substring = malloc(len);
+	ptr_substring = ft_calloc(ft_strlen(s) + 1, 1);
 	if (!ptr_substring)
 		return (NULL);
+	if (len == 0 || start >= ft_strlen(s))
+		return (ptr_substring);
 	while (s[start + i] && len)
 	{
 		ptr_substring[i] = s[start + i];
