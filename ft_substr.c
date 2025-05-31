0246930c70eae9d15e691ced81e0f425b291c6db
sidalope <bisi90@gmail.com>
2025-05-31 19:12:14 +0200

    fix: Change substr to allocate least mem possible

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: abisiani <abisiani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 13:08:42 by abisiani          #+#    #+#             */
-/*   Updated: 2025/05/31 19:02:46 by abisiani         ###   ########.fr       */
+/*   Updated: 2025/05/31 19:11:10 by abisiani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*ptr_substring;
 
 	i = 0;
-	ptr_substring = ft_calloc(ft_strlen(s) + 1, 1);
+	if (len < ft_strlen(s))
+		ptr_substring = ft_calloc(len + 1, 1);
+	else
+		ptr_substring = ft_calloc(ft_strlen(s) + 1, 1);
 	if (!ptr_substring)
 		return (NULL);
 	if (len == 0 || start >= ft_strlen(s))
