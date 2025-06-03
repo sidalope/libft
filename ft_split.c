f83be533e0f1bed329a1d3f73de99c7041dd8dc4
sidalope <bisi90@gmail.com>
2025-06-03 11:26:29 +0200

    add: Add functioning ft_split pending testing

--- ft_split.c
+++ ft_split.c
@@ -6,20 +6,70 @@
 /*   By: abisiani <abisiani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 16:11:20 by abisiani          #+#    #+#             */
-/*   Updated: 2025/05/30 17:29:14 by abisiani         ###   ########.fr       */
+/*   Updated: 2025/06/03 10:28:00 by abisiani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "libft.h"
+
+static size_t	count_strings(char const *s, char c)
+{
+	size_t	i;
+	size_t	count;
+	int		in_word;
+
+	i = 0;
+	count = 0;
+	in_word = 0;
+	while (s[i])
+	{
+		if (!in_word && s[i] != c)
+		{
+			in_word = 1;
+			count++;
+		}
+		if (s[i] == c)
+			in_word = 0;
+		i++;
+	}
+	return (count);
+}
+
+static size_t	wordlen(char const *s, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	return (i);
+}
 
 char	**ft_split(char const *s, char c)
 {
+	size_t	i;
+	size_t	strings_count;
 	char	**strings;
 
-	strings = (char **) malloc(2 * sizeof(char *));
-	(void)s;
-	(void)c;
-	if (strings == NULL)
-		exit(1);
-	return (strings);
+	i = 0;
+	strings_count = count_strings(s, c);
+	strings = (char **) malloc((strings_count + 1) * sizeof(char *));
+	if (!strings)
+		return (NULL);
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			if (!(*strings = malloc(wordlen(s + i, c) + 1)))
+				return (NULL);
+			ft_strlcpy(*strings, s + i, wordlen(s + i, c) + 1);
+			strings++;
+			i += wordlen(s + i, c);
+		}
+		else
+            i++;
+    }
+	*strings = (void *) 0;
+	return (strings -= strings_count);
 }
